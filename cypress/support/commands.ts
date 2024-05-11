8049bbeb8b3efb64372ab7cba0155ad8dc967de5
Earl <earljohnmahiya@gmail.com>
2024-05-11 15:30:40 +0800

    Main (#3)
    * Adding more commands
    
    * TDD just passed
    
    * fix button getByTest

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -46,3 +46,9 @@ Cypress.Commands.add('fillForm',
     })
   },
 )
+
+Cypress.Commands.add('getByTest', (testId) => {
+
+  const log = Cypress.log({name: 'getByTest', message: testId})
+  cy.get(`[data-test="${testId}"]`)
+})
