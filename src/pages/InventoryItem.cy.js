b356e3e2e9c154f69ce545ab9e4760e1626b1c24
EALJ <earljohnmahiya@gmail.com>
2024-05-18 21:13:11 +0800

    InventoryItem with remove item

--- src/pages/InventoryItem.cy.js
+++ src/pages/InventoryItem.cy.js
@@ -1,18 +1,38 @@
 import InventoryItem from './InventoryItem'
 import { InventoryData } from '../utils/InventoryData'
+import { inventoryItem } from '../../cypress/support/pages/inventory-items.page'
+
 
 describe('InventoryItem', () => {
-  it('shows an item', () => {
-    const id = Cypress._.random(0, InventoryData.length - 1)
-    cy.mountWithRouter(<InventoryItem search={'id=' + id} />)
-    const item = InventoryData[id]
 
+  const id = Cypress._.random(0, InventoryData.length - 1)
+  beforeEach(()=> {
+    cy.mountWithRouter(<InventoryItem search={'id=' +id} />)
+  })
+
+  it('shows an item', () => {
+    const item = InventoryData[id]
     cy.get('.inventory_details_desc_container').within(() => {
       cy.contains('.inventory_details_name', item.name)
       cy.contains('.inventory_details_desc', item.desc)
       cy.contains('.inventory_details_price', '$' + item.price)
     })
   })
+
+   it("Inventory Item adds and remove item", { viewportHeight: 1000 }, () => {
+
+    cy.contains('button', 'Add to cart').click()
+    inventoryItem.getCartBadge().should('be.visible')
+    cy.contains('button', 'Remove').click()
+    inventoryItem.getCartBadge().should('not.exist')
+
+
+z
+   })
+
+
+
+
 })
 
 
